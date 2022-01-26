e22f580e2b29af0a263f514f42acf70715bfb709
Ray Orolé <orole.ray@protonmail.com>
2022-01-26 22:34:48 +0100

    Improved bar + logo

--- pages/components/_bar.jsx
+++ pages/components/_bar.jsx
@@ -9,13 +9,21 @@ import {
   CogIcon,
 } from "@heroicons/react/outline";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 function Bar() {
   return (
-    <div className="border-r border-neutral-200 hidden md:block w-72 h-screen">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: "easeOut", duration: 2 }}
+      className="border-r border-neutral-200 hidden md:block w-72 h-screen"
+    >
       <div className="p-4 text-cyan-600 font-semibold text-2xl uppercase flex justify-center items-center">
-        <Image src="/assets/logo.jpg" alt="Logo" width="50" height="50" />
-        <h2>Fuze</h2>
+        <a href="/" className="flex justify-center items-center">
+          <Image src="/assets/logo.jpg" alt="Logo" width="50" height="50" />
+          <h2>Fuze</h2>
+        </a>
       </div>
       <div className="my-3 flex flex-col">
         <div className="mb-4 mx-2">
@@ -23,13 +31,13 @@ function Bar() {
             Algemeen
           </h3>
         </div>
-        <a
+        <motion.a
           href="#"
           className="flex justify-start items-center hover:text-sky-700 hover:bg-neutral-50 border-l-[3px] border-l-transparent hover:border-l-sky-600 text-neutral-700 py-1.5"
         >
           <ChartBarIcon className="w-5 h-5 mx-2" />
           <span className="font-medium">Overzicht</span>
-        </a>
+        </motion.a>
       </div>
       <div className="my-3 flex flex-col">
         <div className="mb-4 mx-2">
@@ -94,7 +102,7 @@ function Bar() {
           <span className="font-medium">Account toevoegen</span>
         </a>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
