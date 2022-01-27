a7b02d7bb2454d89da1e440d9f76470343af4778
robschoovaerts08 <robschoovaerts0@gmail.com>
2022-01-27 13:12:49 +0100

    added aos

--- pages/components/_header.jsx
+++ pages/components/_header.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { MenuIcon } from "@heroicons/react/outline";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
@@ -10,25 +11,30 @@ export default function Header() {
         <meta name="description" content="Fuze online scoreboek" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <header className="p-5 bg-neutral-50 border-b border-b-neutral-100">
-          <nav className="text-stone-600 flex justify-between items-center">
-            <button className="p-2 rounded-full hover:bg-neutral-200"><MenuIcon className="w-6 h-6"/></button>
-            <div className="space-x-8 text-md font-semibold">
-              <a href="#" className="hover:text-stone-700">
-                Resultaten
-              </a>
-              <a href="#" className="hover:text-stone-700">
-                Leerlingcontact
-              </a>
-              <a href="#" className="hover:text-stone-700">
-                Gegevens
-              </a>
-            </div>
-            <button className="px-3 py-2 bg-sky-600 border-transparent hover:text-sky-600 border hover:border-sky-600 hover:bg-neutral-50 transition ease-in-out duration-200 rounded-lg text-white font-semibold text-sm">
-              Uitloggen
-            </button>
-          </nav>
-        </header>
+      <header className="p-5 bg-neutral-50 border-b border-b-neutral-100">
+        <nav className="text-stone-600 flex justify-between items-center">
+          <motion.button
+            whileTap={{ scale: 0.8 }}
+            className="duration-150 p-2 rounded-full hover:bg-neutral-200"
+          >
+            <MenuIcon className="w-6 h-6" />
+          </motion.button>
+          <div className="space-x-8 text-md font-semibold">
+            <a href="#" className="hover:text-stone-700">
+              Resultaten
+            </a>
+            <a href="#" className="hover:text-stone-700">
+              Leerlingcontact
+            </a>
+            <a href="#" className="hover:text-stone-700">
+              Gegevens
+            </a>
+          </div>
+          <button className="px-3 py-2 bg-sky-600 border-transparent hover:text-sky-600 border hover:border-sky-600 hover:bg-neutral-50 transition ease-in-out duration-200 rounded-lg text-white font-semibold text-sm">
+            Uitloggen
+          </button>
+        </nav>
+      </header>
     </>
   );
 }
